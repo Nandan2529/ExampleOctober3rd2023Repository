c754788a6bbe585a33f1547f195ce20e1b5a001f
Nandan2529 <nandansy772@gmail.com>
2023-11-10 18:06:15 +0530

    Test Program

---
+++ Automation/DemoPracticeProject/src/com/sgtesting/p3/Test.java
@@ -0,0 +1,14 @@
+package com.sgtesting.p3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println("5 *"+i+"="+5*i);
+		}
+
+	}
+
+}
