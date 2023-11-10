a95848c3dd1ad3d4014ff5b3629d09406028a001
Nandan2529 <nandansy772@gmail.com>
2023-11-10 18:18:49 +0530

    Test1 Program

---
+++ Automation/DemoPracticeProject/src/com/sgtesting/p3/Test1.java
@@ -0,0 +1,14 @@
+package com.sgtesting.p3;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println("10 *"+i+"="+10*i);
+		}
+	
+	}
+
+}
