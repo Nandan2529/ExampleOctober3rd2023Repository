c2c543678a461f19022b519473bdec6086511417
Nandan2529 <nandansy772@gmail.com>
2023-11-10 17:44:26 +0530

    Sample Program

---
+++ Automation/DemoPracticeProject/src/com/sgtesting/p3/Sample.java
@@ -0,0 +1,15 @@
+package com.sgtesting.p3;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		//Display table
+		for (int i=10;i>=1;i--)
+		{
+		     System.out.println(i);
+		}
+
+	}
+
+}
